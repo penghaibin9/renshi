c9faaa6fc1e66c5c5bca363ce154229074a43fcf
penghaibin9 <penghaibin9@gmail.com>
2026-08-10 18:20:53 +0800

    feat(HR07): restore Django app configuration

---
+++ hr_contracts/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class HrContractsConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "hr_contracts"
+    verbose_name = "HR07 合同管理"
