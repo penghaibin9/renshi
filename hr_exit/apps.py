32357092ac4120ab606ea26d199fd4ad443624c5
penghaibin9 <penghaibin9@gmail.com>
2026-08-10 14:42:28 +0800

    feat(hr16): add Django app config

---
+++ hr_exit/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class HrExitConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "hr_exit"
+    verbose_name = "HR16 退休与离校"
