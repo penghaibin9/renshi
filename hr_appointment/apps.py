bb94e2ac9551df4212f3d572a091f416d832370a
penghaibin9 <penghaibin9@gmail.com>
2026-08-10 14:41:09 +0800

    feat(hr14): add Django app config

---
+++ hr_appointment/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class HrAppointmentConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "hr_appointment"
+    verbose_name = "HR14 岗位聘任"
