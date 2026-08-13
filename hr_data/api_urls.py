c7c2949360e9685527e1782b9e3538f26ea9f9bf
penghaibin9 <penghaibin9@gmail.com>
2026-08-13 08:41:03 +0800

    feat(hr18): route population dimension definition APIs

--- hr_data/api_urls.py
+++ hr_data/api_urls.py
@@ -6,4 +6,14 @@ app_name = "hr_data_api"
 
 urlpatterns = [
     path("dashboard/", api.dashboard, name="dashboard"),
+    path(
+        "definitions/populations/",
+        api.create_population_definition,
+        name="population-definition-create",
+    ),
+    path(
+        "definitions/dimensions/",
+        api.create_dimension_definition,
+        name="dimension-definition-create",
+    ),
 ]
