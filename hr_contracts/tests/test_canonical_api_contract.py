beda4982033f4a4c89c6ed7ba5c27ebee0461578
penghaibin9 <penghaibin9@gmail.com>
2026-08-30 10:03:56 +0800

    test(hr07): verify canonical registry ownership

--- hr_contracts/tests/test_canonical_api_contract.py
+++ hr_contracts/tests/test_canonical_api_contract.py
@@ -1,5 +1,6 @@
 """HR07 canonical API registration and governance contracts."""
 
+from django.test import SimpleTestCase
 from horilla.hr_event_registry import global_event_registry
 from horilla.hr_permission_registry import permission_registry
 
@@ -62,6 +63,7 @@ def test_hr07_permissions_use_global_registry():
     ):
         definition = permission_registry.get(key)
         assert definition.module_code == "HR07"
+        assert definition.key.split(".")[1] == "contracts"
 
 
 def test_hr07_events_use_global_registry():
@@ -74,3 +76,33 @@ def test_hr07_events_use_global_registry():
         definition = global_event_registry.get(event_name, 1)
         assert definition.module_code == "HR07"
         assert definition.aggregate == "agreement"
+        assert definition.name.split(".")[1] == "contracts"
+
+
+class Hr07RegistryContractTests(SimpleTestCase):
+    def test_module_code_and_domain_are_canonical(self):
+        for key in (
+            PERM_AGREEMENT_VIEW,
+            PERM_AGREEMENT_CREATE,
+            PERM_AGREEMENT_SIGN,
+            PERM_AGREEMENT_ACTIVATE,
+            PERM_CASE_CREATE,
+            PERM_CASE_SUBMIT,
+            PERM_CASE_APPROVE,
+            PERM_CASE_SIGN,
+            PERM_CASE_ACTIVATE,
+            PERM_CASE_TERMINATE,
+        ):
+            definition = permission_registry.get(key)
+            self.assertEqual(definition.module_code, "HR07")
+            self.assertEqual(definition.key.split(".")[1], "contracts")
+
+        for event_name in (
+            EVENT_AGREEMENT_CREATED,
+            EVENT_AGREEMENT_SIGNED,
+            EVENT_AGREEMENT_EFFECTIVE,
+            EVENT_AGREEMENT_TERMINATED,
+        ):
+            definition = global_event_registry.get(event_name, 1)
+            self.assertEqual(definition.module_code, "HR07")
+            self.assertEqual(definition.name.split(".")[1], "contracts")
